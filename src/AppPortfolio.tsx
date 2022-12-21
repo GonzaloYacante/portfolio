641f34017a4e1850b08a140772699fa4887606a0
Gonzalo Yacante <gyacante9@gmail.com>
2022-12-21 19:16:49 -0300

    Agregado de iconos en el boton SwitchTheme y arreglo de iconos del footer

--- src/AppPortfolio.tsx
+++ src/AppPortfolio.tsx
@@ -8,22 +8,43 @@ import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 import BackTopButton from "./components/BackTopButton";
 
-import { Layout, Switch } from "antd";
+import sun from "./assets/icons/sun.png";
+import moon from "./assets/icons/moon.png";
+
+import { Layout, Switch, Image } from "antd";
 
 const { Content } = Layout;
 
 function AppPortfolio() {
-  const [theme, setTheme] = useLocalStorage("dark", "theme" ? "light" : "dark");
+  const [theme, setTheme] = useLocalStorage("theme", "theme" ? "dark" : "light");
 
   const switchTheme = () => {
-    const newTheme = theme === "dark" ? "light" : "dark";
+    const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
   };
 
   return (
     <div className="AppPortfolio" data-theme={theme}>
       <div className="content-AppPortfolio">
-        <Switch className="switchTheme" defaultChecked onChange={switchTheme} />
+        <Switch
+          defaultChecked
+          className="switchTheme"
+          onChange={switchTheme}
+          checkedChildren={
+            <Image
+              width={20}
+              src={sun}
+              preview={false}
+            />
+          }
+          unCheckedChildren={
+            <Image
+              width={20}
+              src={moon}
+              preview={false}
+            />
+          }
+        />
         <Layout>
           <NavBar />
           <Content>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
-import { InstagramOutlined, GithubOutlined, LinkedinFilled, CopyrightOutlined, MailOutlined } from '@ant-design/icons';
+import { CopyrightOutlined, MailOutlined, LinkedinOutlined, InstagramOutlined, GithubOutlined } from "@ant-design/icons";
 import { Button, Col, Row, Space, Typography, Popover } from "antd";
-import '../assets/style/components/Footer.less';
+import "../assets/style/components/Footer.less";
 
 const { Text, Paragraph } = Typography;
 
 const content = (
-  <div className='popover_contain'>
-    <Paragraph className='popover_contain-text' copyable>
+  <div className="popover_contain">
+    <Paragraph className="popover_contain-text" copyable>
       gyacante9@gmail.com
     </Paragraph>
   </div>
@@ -15,31 +15,67 @@ const content = (
 function Footer() {
   return (
     <footer>
-      <Row className="contain-footer" justify='center' align="middle">
+      <Row className="contain-footer" justify="center" align="middle">
         <Col>
-          <Space size={20} direction='vertical' align='center'>
+          <Space size={20} direction="vertical" align="center">
             {/* Footer vertical Desktop mode */}
-            <Row className='footer-desktop' justify="center" align="middle">
-              <Space size={30} direction='vertical'>
+            <Row className="footer-desktop" justify="center" align="middle">
+              <Space size={30} direction="vertical">
                 <Col>
-                  <Button className="footer-icons" shape="circle" type='link' href="https://www.instagram.com/gonzaloyacante/" target="blank">
-                    <InstagramOutlined />
+                  <Button
+                    className="footer_contain"
+                    shape="circle"
+                    type="link"
+                    href="https://www.instagram.com/gonzaloyacante/"
+                    target="blank">
+                    <InstagramOutlined
+                      className="footer_contain-icons"
+                      alt="Icono de Instagram"
+                    />
                   </Button>
                 </Col>
                 <Col>
-                  <Button className="footer-icons" ghost shape="circle" type='link' href="https://github.com/GonzaloYacante" target="blank">
-                  <GithubOutlined />
+                  <Button
+                    className="footer_contain"
+                    ghost
+                    shape="circle"
+                    type="link"
+                    href="https://github.com/GonzaloYacante"
+                    target="blank">
+                    <GithubOutlined
+                      className="footer_contain-icons"
+                      alt="Icono de GitHub"
+                    />
                   </Button>
                 </Col>
                 <Col>
-                  <Button className="footer-icons" ghost shape="circle" type='link' href="https://www.linkedin.com/in/gonzaloyacante/" target="blank">
-                    <LinkedinFilled />
+                  <Button
+                    className="footer_contain"
+                    ghost
+                    shape="circle"
+                    type="link"
+                    href="https://www.linkedin.com/in/gonzaloyacante/"
+                    target="blank">
+                    <LinkedinOutlined
+                      className="footer_contain-icons"
+                      alt="Icono de Linkedin"
+                    />
                   </Button>
                 </Col>
                 <Col>
-                  <Popover placement="right" content={content} trigger="click">
-                    <Button className="footer-icons " shape="circle" type='link' href="#">
-                      <MailOutlined />
+                  <Popover
+                    placement="right"
+                    content={content}
+                    trigger="click">
+                    <Button
+                      className="footer_contain"
+                      shape="circle"
+                      type="link"
+                      href="#">
+                      <MailOutlined
+                        className="footer_contain-icons"
+                        alt="Icono de Email"
+                      />
                     </Button>
                   </Popover>
                 </Col>
@@ -47,27 +83,63 @@ function Footer() {
             </Row>
 
             {/* Footer horizontal Mobile mode */}
-            <Row className='footer-mobile' justify="center" align="middle">
+            <Row className="footer-mobile" justify="center" align="middle">
               <Space size={30}>
                 <Col>
-                  <Button className="footer-icons" shape="circle" type='link' href="https://www.instagram.com/gonzaloyacante/" target="blank">
-                    <InstagramOutlined />
+                  <Button
+                    className="footer_contain"
+                    shape="circle"
+                    type="link"
+                    href="https://www.instagram.com/gonzaloyacante/"
+                    target="blank">
+                    <InstagramOutlined
+                      className="footer_contain-icons"
+                      alt="Icono de Instagram"
+                    />
                   </Button>
                 </Col>
                 <Col>
-                  <Button className="footer-icons" ghost shape="circle" type='link' href="https://github.com/GonzaloYacante" target="blank">
-                  <GithubOutlined />
+                  <Button
+                    className="footer_contain"
+                    ghost
+                    shape="circle"
+                    type="link"
+                    href="https://github.com/GonzaloYacante"
+                    target="blank">
+                    <GithubOutlined
+                      className="footer_contain-icons"
+                      alt="Icono de GitHub"
+                    />
                   </Button>
                 </Col>
                 <Col>
-                  <Button className="footer-icons" ghost shape="circle" type='link' href="https://www.linkedin.com/in/gonzaloyacante/" target="blank">
-                    <LinkedinFilled />
+                  <Button
+                    className="footer_contain"
+                    ghost
+                    shape="circle"
+                    type="link"
+                    href="https://www.linkedin.com/in/gonzaloyacante/"
+                    target="blank">
+                    <LinkedinOutlined
+                      className="footer_contain-icons"
+                      alt="Icono de Linkedin"
+                    />
                   </Button>
                 </Col>
                 <Col>
-                  <Popover content={content} trigger="click">
-                    <Button className="footer-icons " shape="circle" type='link' href="#">
-                      <MailOutlined />
+                  <Popover
+                    placement="right"
+                    content={content}
+                    trigger="click">
+                    <Button
+                      className="footer_contain"
+                      shape="circle"
+                      type="link"
+                      href="#">
+                      <MailOutlined
+                        className="footer_contain-icons"
+                        alt="Icono de Email"
+                      />
                     </Button>
                   </Popover>
                 </Col>
@@ -75,15 +147,16 @@ function Footer() {
             </Row>
 
             <Row justify="center" align="middle">
-              <Text strong className='footer-copyright'>
-                2022 <CopyrightOutlined className='footer-copyright' /> Gonzalo Yacante
+              <Text strong className="footer-copyright">
+                2022 <CopyrightOutlined className="footer-copyright" /> Gonzalo
+                Yacante
               </Text>
             </Row>
           </Space>
         </Col>
       </Row>
     </footer>
-  )
+  );
 }
 
 export default Footer;
